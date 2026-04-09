fd8f1808abb3d90ede48698e3013e467a250d4c2
Adam Mogyorosi <adam@fearlessleague.com>
2026-04-09 16:44:11 +0200

    Recover local changes from extracted zip folder

---
+++ _backup_zip_20260409-1644/functions/importing/retrieve_cif_list.py
@@ -0,0 +1,16 @@
+"""
+Created on Mon May 24 17:00:09 2021
+
+@author: DuaneM
+
+Function that downloads all the cif files specified in the protlist.txt in the Input_files map
+"""
+from Bio.PDB import PDBList
+
+def retrieve_cif_list():
+    server = PDBList(server='ftp://ftp.wwpdb.org', pdb='input_files', obsolete_pdb=None ,verbose=True)
+    pdb_list = open('input_files/protlist.txt','r')
+    content = pdb_list.read().split()
+    pdb_list.close()
+
+    server.download_pdb_files(content,pdir="input_files/cif",file_format='mmCif', overwrite=True,obsolete= False)
