6f76771f8b93792da7d131e563baa549095e5ed7
Adam Mogyorosi <adam@fearlessleague.com>
2026-04-09 17:33:59 +0200

    Delete useless stuff

--- Protein CT plugin version 1/plugincontent/utils/config.py
+++
@@ -1,4 +0,0 @@
-SECTION_STYLESHEET = """QGroupBox {font-weight: bold; border: 1px solid palette(dark); border-radius: 6px; margin-top: 6px; padding-top: 4px;}
-QGroupBox::title {subcontrol-origin: margin; left: 10px; padding: 0 3px 0 3px;}"""
-
-INFO_BUTTON_STYLE = "QPushButton { border: none; color: gray; }"
