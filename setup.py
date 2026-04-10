611261db7e31703f8e45074d0988a3bd70c1a91f
materror <matatiesa123@gmail.com>
2026-04-10 14:54:27 +0300

    Reset Adam change 1

--- setup.py
+++ setup.py
@@ -3,11 +3,9 @@ from setuptools import setup, find_packages
 # Setup for the plugin
 setup(
     name="circuit_topology_lacdr",
-    version="0.0",
+    version="v0.0.1",
     description = "PyMOL plugin for Circuit Topology GUI",
     author="LACDR",
     packages=find_packages(),
-    install_requires=['numpy', 'pandas', 'matplotlib', 'dssp', 
-                      'scipy', 'pyqt', 'biopython',
-                      'ipywidgets', 'pip']
+    install_requires=['numpy', 'pandas', 'matplotlib', 'pyqt', 'biopython'],
 )
